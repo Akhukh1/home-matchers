2bcc0183866d1327106d43d1d8b3d98406ec1a58
Aleks Khukh <aleks.khukh@gmail.com>
2023-08-22 18:03:56 +0300

    Задание 2

---
+++ README.md
@@ -0,0 +1,3 @@
+#Tests
+
+[![Build status](https://ci.appveyor.com/api/projects/status/uf2q64pcys3enwew?svg=true)](https://ci.appveyor.com/project/Akhukh1/home-matchers)
---
+++ test/test.js
@@ -0,0 +1,16 @@
+import healthType from '../src/health';
+
+test('health type', () => {
+  const gamerSort = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const gamers = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = healthType(gamers);
+  expect(result).toEqual(gamerSort);
+});
